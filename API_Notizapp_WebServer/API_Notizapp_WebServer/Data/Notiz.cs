77f43820250729f36c659f108b536fe5e8a83331
Amsnir <99396044+Amsnir@users.noreply.github.com>
2023-05-23 22:48:47 +0200

    Add files via upload

---
+++ API_Notizapp_WebServer/API_Notizapp_WebServer/Data/Notiz.cs
@@ -0,0 +1,20 @@
+﻿namespace API_Notizapp_WebServer.Data
+{
+    public class Notiz
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string datum { get; set; }
+        public string inhalt { get; set; }
+
+        public Notiz() { }
+
+        public Notiz(string id, string title, string datum, string inhalt)
+        {
+            this.id = id;
+            this.title = title;
+            this.datum = datum;
+            this.inhalt = inhalt;
+        }
+    }
+}
